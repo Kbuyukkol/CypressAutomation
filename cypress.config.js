417bcfd80854c8d8ca4b827ac10c053089dbdc5e
Kbuyukkol <kamuran.buyukkol@gmail.com>
2023-10-26 10:37:24 +0300

    reporting plugin added

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl: "https://practice.cydeo.com/",
     env: {
@@ -9,7 +10,9 @@ module.exports = defineConfig({
     video: false,
     retries: 1,
     defaultCommandTimeout: 5000,
+    
     setupNodeEvents(on, config) {
+    require('cypress-mochawesome-reporter/plugin')(on); 
       // implement node event listeners here
     },
   },
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,6 +16,8 @@
 // Import commands.js using ES2015 syntax:
 import './commands';
 
+import 'cypress-mochawesome-reporter/register';
+
 Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from
     // failing the test
